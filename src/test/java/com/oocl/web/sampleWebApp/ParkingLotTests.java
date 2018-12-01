0c14c987cab17e0e2cecea89b97c063861177d73
Mo Hon Sing <mohonsing113@gmail.com>
2018-12-01 10:27:22 +0800

    test: add parkingLot exceedLimit test

--- src/test/java/com/oocl/web/sampleWebApp/ParkingLotTests.java
+++ src/test/java/com/oocl/web/sampleWebApp/ParkingLotTests.java
@@ -68,4 +68,19 @@ public class ParkingLotTests {
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", containsString("/parkinglots/"+newParkingLotId)));
     }
+
+    @Test
+    public void should_throw_exception_due_to_exceed_employeeid_length() throws Exception {
+        //given
+        String newParkingLotId = "lot";
+        int newParkingLotCapacityExceedTheMaxLimit = 1000;
+        String newParkingLotInJson = "{\"parkingLotId\":\"" + newParkingLotId + "\", \"capacity\":"+newParkingLotCapacityExceedTheMaxLimit+"}";
+
+        //when
+        mvc.perform(post("/parkinglots")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(newParkingLotInJson)
+        )//then
+                .andExpect(status().isBadRequest());
+    }
 }
